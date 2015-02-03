6592d9bd3a163c65ab0dce6747f5c611ed8d2bc6
Clint <clintp1@gmail.com>
2015-02-03 14:21:07 +0000

    added first test gutter game score should be zero

---
+++ spec/gameSpec.js
@@ -0,0 +1,14 @@
+var Game = require('../src/game');
+
+describe('Gutter game', function(){
+
+  var  game = new Game();
+
+  it('Scores 0 on a gutter game', function(){
+    for(var i = 0; i < 20; i++){
+      game.roll(0);
+    }
+    expect(game.score).toEqual(0);
+  });
+
+});
